f420ed2e64cc0f4e402574e39e2f29749f731f57
lgsp_Harold <lgsp_leslie@126.com>
2021-04-19 21:22:00 +0800

    优化静态页面

--- app.py
+++ app.py
@@ -1,8 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='assets')
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    """ 首页 """
+    return render_template('index.html')
+
+
+@app.route('/follow')
+def follow():
+    """ 关注 """
+    return render_template('follow.html')
+
+
+@app.route('/detail')
+def detail():
+    """ 详情 """
+    return render_template('detail.html')
+
+
+@app.route('/login')
+def login():
+    """ 登录 """
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    """ 注册 """
+    return render_template('register.html')
+
+
+@app.route('/mine')
+def mine():
+    """ 个人中心 """
+    return render_template('mine.html')
+
+
+@app.route('/write')
+def write():
+    """ 提问、回答 """
+    return render_template('write.html')
