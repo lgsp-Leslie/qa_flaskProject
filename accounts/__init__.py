9f4907f932efd23c9d7e2ef858dd7e1ca642d495
lgsp_Harold <lgsp_leslie@126.com>
2021-04-21 16:00:35 +0800

    创建数据库模型，并使用蓝图改进项目

---
+++ accounts/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# Version:python3.6.1
+# Project:qa_flaskProject
+# File:__init__.py.py
+# Data:2021/4/21 15:46
+# Author:LGSP_Harold
+
+
+if __name__ == '__main__':
+    pass
--- app.py
+++ app.py
@@ -1,46 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
+
+from models import db
+from accounts.views import accounts
+from qa.views import qa
 
 app = Flask(__name__, static_folder='assets')
+# 从配置文件加载配置
+app.config.from_object('conf.Config')
 
+# 数据库初始化
+db.init_app(app)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    """ 首页 """
-    return render_template('index.html')
-
-
-@app.route('/follow')
-def follow():
-    """ 关注 """
-    return render_template('follow.html')
-
-
-@app.route('/detail')
-def detail():
-    """ 详情 """
-    return render_template('detail.html')
-
-
-@app.route('/login')
-def login():
-    """ 登录 """
-    return render_template('login.html')
-
-
-@app.route('/register')
-def register():
-    """ 注册 """
-    return render_template('register.html')
-
-
-@app.route('/mine')
-def mine():
-    """ 个人中心 """
-    return render_template('mine.html')
-
-
-@app.route('/write')
-def write():
-    """ 提问、回答 """
-    return render_template('write.html')
+# 注册蓝图
+app.register_blueprint(accounts, url_prefix='/accounts')
+app.register_blueprint(qa, url_prefix='/')
